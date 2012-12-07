68b3343acc408168b93393b76c7454e84341ec5e
Scott Barstow <sbarstow@gmail.com>
2012-12-07 16:07:54 -0500

    removing tests.js from the tree, http coding added to switchcoder.js

--- tests.js
+++
@@ -1,4 +0,0 @@
-var switchCoder = require('./index.js')
-
-var client = new switchCoder.Client("123", "http://test.com");
-client.getScript(123, new switchCoder.PhoneNumber("12345"), null).invoke();
